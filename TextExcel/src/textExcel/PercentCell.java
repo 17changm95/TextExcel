9e10d888d6117bdb0593dd93dc2966d613361eb2
17changm95 <17changm95@millshighsch.org>
2017-03-02 08:22:41 -0800

    Added all the cell classes

---
+++ TextExcel/src/textExcel/PercentCell.java
@@ -0,0 +1,12 @@
+package textExcel;
+
+public class PercentCell implements Cell {
+
+	public String abbreviatedCellText() {
+		return "";
+	}
+	
+	public String fullCellText() {
+		return "";
+	}
+}
---
+++ TextExcel/src/textExcel/RealCell.java
@@ -0,0 +1,12 @@
+package textExcel;
+
+public class RealCell implements Cell {
+
+	public String abbreviatedCellText() {
+		return "";
+	}
+	
+	public String fullCellText() {
+		return "";
+	}
+}

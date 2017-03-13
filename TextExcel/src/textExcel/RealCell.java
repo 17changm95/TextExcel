e257c8b5d9b9138804520f511f842be5ccdc9808
17changm95 <17changm95@millshighsch.org>
2017-03-13 02:50:39 -0700

    More CHeckpoknt 2

--- TextExcel/src/textExcel/RealCell.java
+++ TextExcel/src/textExcel/RealCell.java
@@ -2,6 +2,12 @@ package textExcel;
 
 public class RealCell implements Cell {
 
+	private String realNum;
+	
+	public RealCell(String realNum) {
+		this.realNum = realNum;
+	}
+	
 	public String abbreviatedCellText() {
 		return "";
 	}
